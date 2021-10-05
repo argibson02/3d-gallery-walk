e232f8312319aa4d9a8d853c98778d4540443e0f
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-04 21:53:50 -0700

    added render frame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const near = 0.1;
 const far = 1000;
 
 /**
- * @description Initialize a Three JS Scene, camera, and renderer. Returns the scene created.
+ * @description Initialize a Three JS Scene, camera, and renderer. Returns the scene created. Note that the element must be sized BEFORE this is run.
  * @param {String} elementId The jQuery selector used for the given container of the preview "#element-id"
  * 
  * @returns {Array} [scene, camera, renderer]
@@ -15,7 +15,7 @@ function initPreview( elementId ) {
     let containerWidth = $( elementId ).width();
     let containerHeight = $( elementId ).height();
 
-    console.log(containerWidth + ", " + containerHeight);
+    // console.log(containerWidth + ", " + containerHeight);
 
     // Create the scene, camera, and renderer needed for three.js
     scene = new THREE.Scene();
@@ -30,7 +30,26 @@ function initPreview( elementId ) {
     // Set the size of the renderer and add it to the page
     renderer.setSize( containerWidth, containerHeight );
     document.querySelector( elementId ).appendChild( renderer.domElement );
-
+    
+    return [scene, camera, renderer];
 }
 
-initPreview("#preview");
+// Test - Carousel container should be black
+var sceneCameraRenderer = initPreview("#preview");
+
+/**
+ * @description Renders a new frame to the page
+ * @param {Array} sceneCameraRenderer An array of the Three Scene, Camera, and Renderer
+ */
+function renderFrame( sceneCameraRenderer ) {
+    let scene, camera, renderer;
+    scene = sceneCameraRenderer[0];
+    camera = sceneCameraRenderer[1];
+    renderer = sceneCameraRenderer[2];
+
+    renderer.render( scene, camera );
+}
+
+// Test - carousel container should be grey
+sceneCameraRenderer[0].background = new THREE.Color( 0xCCCCCC );
+renderFrame(sceneCameraRenderer);
