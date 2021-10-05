3140c5426c6f2dc29fe7991dd9bc12b4127c1e57
Alex Gibson <argibson02@gmail.com>
2021-10-05 09:01:41 -0700

    Added Object info retrieval on all search results.

--- assets/js/script.js
+++ assets/js/script.js
@@ -165,7 +165,7 @@ var urlSearchOnNumEx = "https://collectionapi.metmuseum.org/public/collection/v1
 
 var objectNumArray = [];
 var searchUrlArray = [];
-
+var artSearchObj = [];
 
 
 //////////////////////////////////////////////////////////////////  Get Object ID urls on search
@@ -184,13 +184,43 @@ function getObjectNum() {
                     //console.log(tempObjectNumIds);
                     searchUrlArray.push(tempObjectNumIds); // pushes each to an array to hold the urls
                 }
-                if (searchUrlArray.length > 100) { // trims off search results yielding more than 100 results to reduce process time
-                    searchUrlArray = searchUrlArray.slice(0, 100);
+                if (searchUrlArray.length > 80) { // trims off search results yielding more than 80 results to reduce process time. The API limits requests to 80 per second
+                    searchUrlArray = searchUrlArray.slice(0, 80);
                 }
-                //console.log(searchUrlArray);
+                console.log(searchUrlArray);
+
+                for (i = 0; i < searchUrlArray.length; i++) {
+                    fetch(searchUrlArray[i]) // fetches data for each object 
+                        .then(function (response) {
+                            //console.log(response);
+                            return response.json();
+                        })
+                        .then(function (artData) {
+                            //console.log(artData); // console log for all data on object
+                            var tempArtObj =
+                            {
+                                "title": artData.title,  // i.e. "La Berceuse (Woman Rocking a Cradle; Augustine-Alix Pellicot Roulin, 1851–1930)"
+                                "artistDisplayName": artData.artistDisplayName,  // i.e. "Vincent van Gogh"
+                                "artistNationality": artData.artistNationality, // i.e. "Dutch"
+                                "artistDisplayBio": artData.artistDisplayBio, // i.e. "Dutch, Zundert 1853–1890 Auvers-sur-Oise"
+                                "department": artData.department,  // i.e. "European Paintings"
+                                "primaryImage": artData.primaryImage,  // i.e. "https://images.metmuseum.org/CRDImages/ep/original/DP-19279-001.jpg"
+                                "primaryImageSmall": artData.primaryImageSmall,  // i.e. "https://images.metmuseum.org/CRDImages/ep/web-large/DP-19279-001.jpg"
+                                "classification": artData.classification, // i.e. "Paintings"
+                                "objectDate": artData.objectDate,  // i.e. "1889" ~string
+                                "objectBeginDate": artData.objectBeginDate, // i.e. 1889 ~num
+                                "objectEndDate": artData.objectEndDate, // i.e. 1889 ~num
+                                "medium": artData.medium, // i.e. "Oil on canvas"
+                                "objectURL": artData.objectURL, // i.e. "https://www.metmuseum.org/art/collection/search/437984"
+                                "culture": artData.culture, // i.e. "" (often null for paintings)
+                            }
+                            //console.log(tempArtObj);  // console log the data we are interested in
+                            artSearchObj.push(tempArtObj);
+                        });
+                }
             }
             else {
-                //------ add no search results found function and actions here
+                //------ add 0 search results found function and actions here
                 return;
             }
         })
@@ -208,13 +238,66 @@ getObjectNum();
 
 
 
-$(".cityBtn").on("click", function () { 
-    for (i = 0; i < objectNumArray.length; i++) {
-        tempSearchOnNum = collectionAPIRoot + objectNumArray[i];
-        console.log(tempSearchOnNum);
-        searchUrlArray.push(tempSearchOnNum);
-    }
-    console.log(searchUrlArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//////////////////////////////////////////////////////////////////  Get Object ID urls on search - stand alone
+function getObjectNum() {
+
+    fetch(urlSearchEx) //----- need to swap this out for a variable that pulls and cleans a search inputs from a search field
+        .then(function (response) { // fetches objects from search API
+            return response.json();
+        })
+        .then(function (data) {
+            objectNumIds = data.objectIDs; // puts returned object id numbers in an array
+            objectNumTotal = data.total; // number of total search results 
+            if (objectNumTotal > 0) { // checks to see if the query returned any results
+                for (i = 0; i < objectNumIds.length; i++) {
+                    var tempObjectNumIds = collectionAPIRoot + objectNumIds[i]; // for each, append the object id number to the end of a collection API root
+                    //console.log(tempObjectNumIds);
+                    searchUrlArray.push(tempObjectNumIds); // pushes each to an array to hold the urls
+                }
+                if (searchUrlArray.length > 80) { // trims off search results yielding more than 100 results to reduce process time. The API limits requests to 80 per second
+                    searchUrlArray = searchUrlArray.slice(0, 80);
+                }
+                //console.log(searchUrlArray);
+            }
+            else {
+                //------ add no search results found function and actions here
+                return;
+            }
+        })
+}
+getObjectNum();
+////////////////////////////////////////////////////////////////
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$("<button>").on("click", function () { 
+    console.log(artSearchObj);
 });
 
 
